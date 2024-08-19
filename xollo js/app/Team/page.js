17f2df394f48a29ebcc0ade01c07d8d36113e251
Majorplaydev <victorayoonileere@gmail.com>
2024-08-19 17:11:03 +0100

    Page routing and file structure

---
+++ xollo js/app/Team/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-cyan-300">
+      <div className="flex items-center justify-center w-full ">
+        <h1 className="text-6xl text-orange-800"> Meet the team </h1>
+      </div>
+    </main>
+  )
+}
+
+export default page
