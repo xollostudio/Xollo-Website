4ffcb7ad24fa98c918caeb23722df3145166f459
Majorplaydev <victorayoonileere@gmail.com>
2024-08-22 17:00:19 +0100

    Hompage, globals styles, assets... complete asset will be on next push

---
+++ xollo js/app/Components/Box.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='box-two'></div>
+  )
+}
+
+export default Box
--- xollo js/app/layout.js
+++ xollo js/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
