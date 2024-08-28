30287966caad0f715453d373342c22567adc5363
Tessyrich <olyrich3@gmail.com>
2024-08-28 14:03:46 +0100

    Add Contact Page

--- xollo js/app/Contacts/page.js
+++ xollo js/app/Contacts/page.js
@@ -2,9 +2,57 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-indigo-300">
-      <div className="flex items-center justify-center w-full ">
-        <h1 className="text-6xl text-orange-800"> CONTACT US</h1>
+    <main className="flex min-h-screen font-['Archivo-thin'] flex-col items-center  justify-between p-24">
+      <div className="flex items-center justify-start w-full py-16">
+        <h1 className="xl:text-8xl text-xl w-2/3 "> Hey! Tell us about your project</h1>
+      </div>
+      <div className='w-full'>
+        <p className='py-10 xl:text-2xl text-xl '>I'm inetrested in....</p>
+        <div className='flex flex-col'>
+          <p className='text-sm text-gray-400'>
+            Development
+          </p>
+          <div className='flex xl:text-2xl text-xl gap-4'>
+            <button>Website redesign</button>
+            <button>UI/UX Design</button>
+            <button>Logo Design</button>
+          </div>
+        </div>
+        <div className='flex flex-col py-6'>
+          <p className='text-sm text-gray-400'>
+            Design
+          </p>
+          <div className='flex xl:flex-row flex-col gap-4'>
+          <div className='flex xl:text-2xl text-xl gap-4'>
+            <button>Branding/Brand Identity Design</button>
+            <button>Logo Re-design</button>
+          </div>
+          <div className='flex xl:text-2xl text-xl gap-4'>
+            <button>Motion Design</button>
+            <button>Art</button>
+            <button>Illustration</button>
+          </div>
+          </div>
+        </div>
+        <div className='flex flex-col py-6'>
+          <p className='text-sm text-gray-400'>
+            Art/Illustration
+          </p>
+          <div className='flex xl:text-2xl text-xl gap-4'>
+            <button>Brand Re-design</button>
+            <button>Site from scratch</button>
+          </div>
+          
+          <p className='py-10 xl:text-2xl text-xl '>I am.....</p>
+        </div>
+      </div>
+      <div className='w-full flex flex-col'>
+      <input type='text' placeholder='My name is:' className=' border-b p-4 w-full focus:border-none my-8'/>
+      <input type='email' placeholder='Email' className=' border-b p-4 w-full focus:border-none my-8'/>
+      <div className='flex mt-8'>
+      <input type='text' placeholder='About my project' className=' border-b p-4 w-2/3 focus:outline-none mr-4'/>
+      <input type='text' placeholder='Additional Details' className=' border-b p-4 w-1/3 focus:outline-none '/>
+      </div>
       </div>
     </main>
   )
--- xollo js/app/WhatWeDo/page.js
+++ xollo js/app/WhatWeDo/page.js
@@ -1,13 +1,83 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
 
 const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-300">
-      <div className="flex items-center justify-center w-full ">
-        <h1 className="text-6xl text-orange-800"> WHAT WE DO</h1>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+        <div className=" min-h-screen flex flex-col  justify-between w-full font-['Archivo-thin'] border-b-4 py-10">
+          <h1 className="xl:text-8xl text-xl mb-4 w-2/3">
+            {" "}
+            We create memorable websites
+          </h1>
+          <p className="w-full text-center">Xollo Studio</p>
+          <h1 className="xl:text-8xl text-xl mb-4 text-start">
+            {" "}
+            Here's what we do
+          </h1>
+        </div>
+      </main>
+      <div className="h-[40vh] flex xl:flex-row flex-col justify-end px-16">
+        <div className="h-full w-full xl:w-1/2  flex gap-10">
+          <p className="w-1/3">(CREATIVE DESIGN)</p>
+          <p className="w-2/3">
+            We don't do cookie-cutter solutions. Our user-centered design
+            encourages productivity and boosts revenue.
+          </p>
+        </div>
       </div>
-    </main>
-  )
-}
+      <div className="flex flex-col px-16 rounded-2xl py-16 bg-[#F6F6F6] font-['Archivo-thin']">
+        <div className="h-[60vh] flex justify-start items">
+          <h1 className="xl:text-8xl text-xl mb-4 text-start w-full xl:w-1/2">
+            {" "}
+            Smart development
+          </h1>
+        </div>
+        <div className="w-full h-[40vh] bg-[#CECECE] flex items-start xl:justify-end p-16 rounded-3xl">
+          <p className="xl:w-1/4 w-full">
+            We don't do cookie-cutter solutions. Our user-centered design
+            encourages productivity and boosts revenue.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="p-16  xl:h-[90vh] h-[30vh] flex items-start justify-start border-b-4">
+          <h1 className="xl:text-8xl text-xl mb-4 text-start w-full font-['Archivo-thin']">
+            {" "}
+            Design and branding
+          </h1>
+        </div>
+        <div className="h-[40vh] flex xl:flex-row flex-col justify-end px-16">
+        <div className="h-full w-full xl:w-1/2  flex gap-10">
+          <p className="w-1/3">(CREATIVE DESIGN)</p>
+          <p className="w-2/3">
+            Combining unique design and rich technology, we build digital
+            products exactly as they were designed, without shortcuts or
+            simplifications.
+          </p>
+        </div>
+        </div>
+        <div className="flex flex-col px-16 rounded-2xl py-16 bg-[#F6F6F6] font-['Archivo-thin']">
+        <div className="h-[60vh] flex justify-start items">
+          <h1 className="xl:text-8xl text-xl mb-4 text-start w-full">
+            {" "}
+            Art and illustrations
+          </h1>
+        </div>
+        <div className="w-full h-[40vh] bg-[#CECECE] flex items-start xl:justify-end p-16 rounded-3xl">
+          <p className="xl:w-1/4 w-full">
+            We don't do cookie-cutter solutions. Our user-centered design
+            encourages productivity and boosts revenue.
+          </p>
+        </div>
+      </div>
+      <div className="h-[50vh] flex justify-center items-center font-['Archivo-thin']">
+        <p className="xl:text-8xl text-xl mb-4 text-start w-full xl:w-1/2">Work with us</p>
+      </div>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
